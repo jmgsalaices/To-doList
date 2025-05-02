05f5523028eeed2d1a7ed9034430b5e6e447d4a7
J Mauricio G Salaices <51425235+jmgsalaices@users.noreply.github.com>
2025-05-02 03:49:58 -0600

    Adding viewModels (MVVM)

---
+++ To-doList/ViewModels/RelayCommand.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace To_doList.ViewModels
+{
+    public class RelayCommand : ICommand
+    {
+        public readonly Action _execute;
+        public readonly Func<bool> _canExecute;
+        public RelayCommand(Action execute, Func<bool> canExecute) 
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = canExecute;
+        }     
+        
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+          
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null || _canExecute();
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute();   
+        }
+    }
+
+    public class RelayCommand<T> : ICommand
+    {
+        public readonly Action _execute;
+        public readonly Func<bool> _canExecute;
+        public RelayCommand(Action execute, Func<bool> canExecute)
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = canExecute;
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null || _canExecute();
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute();
+        }
+    }
+}
