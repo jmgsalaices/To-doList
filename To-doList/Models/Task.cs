8bd61318b0967bf5d46b8237aab66df949fde4f3
J Mauricio G Salaices <51425235+jmgsalaices@users.noreply.github.com>
2025-05-02 02:53:56 -0600

    Added Models
    Added (MVVM) Model class Task
     *all related with the item task fields
        Id
        Title
        Description
        DueDate
        StartDate
        IsComplete .........etc.
      *CAtegories, Importance
    Adding also the EA diagram Files Requirimnt Funtional ToDoList

---
+++ To-doList/Models/Task.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace To_doList.Models
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime DueDate { get; set; }
+        public DateTime StartDate { get; set; }
+        public bool IsComplete { get; set; }
+        public TimeSpan Timer { get; set; }
+        public TaskState TaskState { get; set; }
+        public TaskCategory TaskCategory { get; set; }
+        public TaskImportance TaskImportance { get; set; }
+    }
+
+    public enum TaskState
+    {
+        /// <summary>
+        /// The Task is still in progress
+        /// </summary>
+        InProgress,
+        /// <summary>
+        /// The Task has been marked completed
+        /// </summary>
+        Complete,
+        /// <summary>
+        /// The task hasn't been started yet
+        /// </summary>
+        Notstarted,
+        Late,
+        Archived,
+        Deleted
+    }
+    public enum TaskCategory
+    {
+        Work,
+        Personal,
+        Home,   
+        HealtWebelbeing,
+        Finance,
+        Shoping,
+        SocialFamily,
+        Education,
+        Travel,
+        Errands,
+        HobbiesLeisure,
+        VolunteeringCommunity,
+        BirthDayAnniversaries,
+        Porjects,
+        LongTermsGoals 
+    }
+    public enum TaskImportance
+    { 
+        Low,
+        Medium,
+        High,
+        Critical
+    }
+}
